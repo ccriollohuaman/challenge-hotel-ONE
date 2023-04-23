9e213c0b3fa1258484621fa9d4957d08b6f1b1b1
Cristhian <83378496+ccriollohuaman@users.noreply.github.com>
2023-04-23 18:43:55 -0500

    Se crea clase Main para ejecutar la aplicación

---
+++ challenge-hotel-ONE/src/jdbc/Main.java
@@ -0,0 +1,21 @@
+package jdbc;
+
+import java.awt.EventQueue;
+
+import jdbc.views.MenuPrincipal;
+
+public class Main {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipal frame = new MenuPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+}
