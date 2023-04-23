1c0bded893c0161f0d921efe00113ce5de6047cd
Cristhian <83378496+ccriollohuaman@users.noreply.github.com>
2023-04-23 18:19:42 -0500

    Se crea las clases modelo de las tablas huesped y reserva

---
+++ challenge-hotel-ONE/src/jdbc/model/Huesped.java
@@ -0,0 +1,15 @@
+package jdbc.model;
+
+import java.sql.Date;
+
+public class Huesped {
+
+	private Integer id;
+	private String nombre;
+	private String apellido;
+	private Date fechaNacimiento;
+	private String nacionalidad;
+	private String telefono;
+	private Integer reservaId;
+	
+}
---
+++ challenge-hotel-ONE/src/jdbc/model/Reserva.java
@@ -0,0 +1,14 @@
+package jdbc.model;
+
+import java.sql.Date;
+
+public class Reserva {
+	
+	private Integer id;
+	private Date fechaEntrada;
+	private Date fechaSalida;
+	private Float valor;
+	private String formaPago;
+	
+
+}
