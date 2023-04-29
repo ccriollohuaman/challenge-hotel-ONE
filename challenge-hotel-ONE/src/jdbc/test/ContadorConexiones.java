cd84be4145016656244385e9f639aa797196de81
Cristhian <83378496+ccriollohuaman@users.noreply.github.com>
2023-04-29 18:18:32 -0500

    Se agrega paquete test para pruebas, y en el, un contador de conexiones

---
+++ challenge-hotel-ONE/src/jdbc/test/ContadorConexiones.java
@@ -0,0 +1,27 @@
+package jdbc.test;
+
+public class ContadorConexiones {
+	
+	private static ContadorConexiones instance = null;
+    private int conexion = 0;
+    
+    public ContadorConexiones() {}
+    
+    public static ContadorConexiones getInstance() {
+        if (instance == null) {
+            instance = new ContadorConexiones();
+        }
+        return instance;
+    }
+    
+    public synchronized void increment() {
+    	conexion++;
+        System.out.println("Número de conexiones: " + conexion);
+    }
+    
+    public synchronized void decrement() {
+    	conexion--;
+        System.out.println("Número de conexiones: " + conexion);
+    }
+
+}
